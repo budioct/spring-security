1166c8256efb5fed6e5c002678648cd2ae269bdd
Budhi Octaviansyah <budioct@gmail.com>
2023-08-09 12:55:32 +0700

    start project

---
+++ src/main/java/com/tutorial/BelajarSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarSpringSecurityApplication.class, args);
+	}
+
+}
