dfce91164faa0d75376e71bbda5d5f1aa7c4f158
Budhi Octaviansyah <budioct@gmail.com>
2023-08-10 15:27:55 +0700

    testing entity manager factory 2nd

---
+++ src/test/java/com/tutorial/entitymanagerfactorytest/EntityManagerFactoryTest.java
@@ -0,0 +1,30 @@
+package com.tutorial.entitymanagerfactorytest;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class EntityManagerFactoryTest {
+
+    @Autowired
+    EntityManagerFactory entityManagerFactory;
+
+
+    @Test
+    void testEntityManager(){
+
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+
+        transaction.commit();
+        entityManager.close();
+        entityManagerFactory.close();
+
+    }
+
+}
