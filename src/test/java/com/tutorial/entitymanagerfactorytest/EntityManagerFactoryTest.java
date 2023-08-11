75df90d1a0b65644dd2c6740debe72cf8c983e9f
Budhi Octaviansyah <budioct@gmail.com>
2023-08-11 10:46:32 +0700

    test user authentication, http get endpoint with authorites

--- src/test/java/com/tutorial/entitymanagerfactorytest/EntityManagerFactoryTest.java
+++ src/test/java/com/tutorial/entitymanagerfactorytest/EntityManagerFactoryTest.java
@@ -1,17 +1,25 @@
 package com.tutorial.entitymanagerfactorytest;
 
+import com.tutorial.entity.User;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Slf4j
 @SpringBootTest
 public class EntityManagerFactoryTest {
 
     @Autowired
     EntityManagerFactory entityManagerFactory;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
 
 
     @Test
@@ -26,5 +34,27 @@ public class EntityManagerFactoryTest {
         entityManagerFactory.close();
 
     }
+
+    @Test
+    void testFindtUsername(){
+
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+
+        User user = entityManager.find(User.class, 1);
+        Assertions.assertEquals("budhi", user.getUsername());
+
+        log.info("username== {}", user.getUsername());
+        log.info("authorities== {}", user.getAuthorities());
+
+
+        transaction.commit();
+        entityManager.close();
+        entityManagerFactory.close();
+
+    }
+
+
 
 }
